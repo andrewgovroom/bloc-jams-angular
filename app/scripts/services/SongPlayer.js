3c9f6a0bdd62523c5ddd6ca43decc38d2c59bc54
Andrew <andrewgovroom@hotmail.com>
2017-02-23 22:10:33 -0800

    Fixed Public Methods in Song Player.js

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -8,42 +8,43 @@
  * @type {Object}
  */
          var currentBuzzObject = null;
-         
-         SongPlayer.play = function(song) {
-             if (currentSong !== song) {
-                 
+        
                  /**
  * @function setSong
  * @desc Stops currently playing song and loads new audio file as currentBuzzObject
  * @param {Object} song
  */  
                  
-                 var setSong = function(song) {
-                    if (currentBuzzObject) {
-                            currentBuzzObject.stop();
-                            currentSong.playing = null;
-                        }
- 
-                        currentBuzzObject = new buzz.sound(song.audioUrl, {
-                                formats: ['mp3'],
-                                preload: true
-                        });
- 
-                     currentSong = song;
-                };
+         var setSong = function(song) {
+            if (currentBuzzObject) {
+                    currentBuzzObject.stop();
+                    currentSong.playing = null;
+                }
+
+                currentBuzzObject = new buzz.sound(song.audioUrl, {
+                        formats: ['mp3'],
+                        preload: true
+                });
+
+             currentSong = song;
+         };
                  
-                 setSong(song);  
-                 currentBuzzObject.play();
-                 song.playing = true;
+         SongPlayer.play = function(song) {
+            if (currentSong !== song) {
+             setSong(song);  
+             currentBuzzObject.play();
+             song.playing = true;
+                 
         
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
                    
-             } 
+                } 
             
             }      
          };
+                 
 
         SongPlayer.pause = function(song) {
         currentBuzzObject.pause();
